fe50bc0dc56420072b47fc474924d2624db8c2ab
alxcl12 <albualexandru00@gmail.com>
2021-04-07 13:05:47 +0300

    Finished chapter 2, all set up being done.

---
+++ shop/src/main/java/ro/msg/learning/shop/HelloController.java
@@ -0,0 +1,16 @@
+package ro.msg.learning.shop;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
